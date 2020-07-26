3e3b4169e326a36035ca6c55f7988ba95a6f7513
sigu-qucnd <evt.sgtmt@gmail.com>
2020-07-27 05:00:05 +0900

    Initial Commit

---
+++ consumption-tax/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  consumption-tax
+//
+//  Created by Ryo Nagasawa on 2020/07/27.
+//  Copyright © 2020 Ryo Nagasawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
